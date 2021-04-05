8adbbaa8331b4572c7910b7d593c524ce3993d16
CSC466-hub <70179088+CSC466-hub@users.noreply.github.com>
2021-04-05 10:18:33 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -39,3 +39,26 @@ for i in range(num_nodes):
   node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/setupSaucy.sh"))
 pc.printRequestRSpec(request)
+"""
+import geni.portal as portal
+import geni.rspec.pg as rspec
+
+# Create a Request object to start building the RSpec.
+request = portal.context.makeRequestRSpec()
+# Create a XenVM
+node = request.RawPC("node")
+#urn:publicid:IDN+utah.cloudlab.us+image+emulab-ops//UBUNTU20-64-ARM
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-ARM"
+node.routable_control_ip = "true"
+
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command="sudo apt update"))
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command="sudo apt install -y apache2"))
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command='sudo ufw allow in "Apache Full"'))
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command='sudo systemctl status apache2'))
+# Print the RSpec to the enclosing page.
+portal.context.printRequestRSpec()
+"""
