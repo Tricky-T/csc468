59044277ee3a226d042faed59cfd1634322df5ef
CSC466-hub <70179088+CSC466-hub@users.noreply.github.com>
2021-04-14 10:26:19 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -20,6 +20,48 @@ request.addTour(tour)
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
+num_nodes = 1
+for i in range(num_nodes):
+  if i == 0:
+    node = request.RawPC("node")
+  else:
+    node = request.XenVM("worker-" + str(i))
+  #node.cores = 12#node.cores = 4
+  #node.ram = 32768#node.ram = 8192
+  node.routable_control_ip = "true" 
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+  
+  # setup Docker
+  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+pc.printRequestRSpec(request)
+
+
+"""import geni.portal as portal
+import geni.rspec.pg as pg
+import geni.rspec.igext as IG
+
+pc = portal.Context()
+request = pc.makeRequestRSpec()
+
+tourDescription = \
+""
+This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
+""
+
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
+
 num_nodes = 3
 for i in range(num_nodes):
   if i == 0:
@@ -39,7 +81,7 @@ for i in range(num_nodes):
   node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/setupSaucy.sh"))
 pc.printRequestRSpec(request)
-"""
+""
 import geni.portal as portal
 import geni.rspec.pg as rspec
 
