87100568973e88421dfe7451308a6b4e213bde86
CSC466-hub <70179088+CSC466-hub@users.noreply.github.com>
2021-05-11 21:32:07 -0400

    Update doit.sh

--- nuttcp/doit.sh
+++ nuttcp/doit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install nuttcp
-sudo apt-get install linux-tools-common
-sudo apt-get install linux-tools-generic
-sudo apt-get install linux-cloud-tools-generic
+sudo apt-get install -y nuttcp
+sudo apt-get install -y linux-tools-common
+sudo apt-get install -y linux-tools-generic
+sudo apt-get install -y linux-cloud-tools-generic
 
-sudo apt-get install linux-tools-4.15.0-137-generic
+sudo apt-get install -y linux-tools-4.15.0-137-generic
 
-for ((i=0;i<20;i++)); do ./linux.sh; done
-for ((i=0;i<20;i++)); do ./docker.sh; done
+for ((i=0;i<20;i++)); do sudo bash linux.sh; done
+for ((i=0;i<20;i++)); do sudo bash docker.sh; done
